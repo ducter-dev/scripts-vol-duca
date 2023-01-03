868714409de456eeb6893f16b8ad44712fa11bc6
Ducter-dev <michel.morales@ducter.com.mx>
2023-01-03 12:26:37 -0600

    VOL-36: Modelado de datos para tendencias

---
+++ proyect/__init__.py
@@ -0,0 +1,26 @@
+from email.errors import InvalidMultipartContentTransferEncodingDefect
+from turtle import bgcolor
+from datetime import datetime, timedelta
+from .balancesBD import cnx as conection
+from .balancesBD import Balance, Contenedor, EntradaDetalle, BalanceMovimiento, ContenedorMovimiento, ContenedorBalance, SalidaDetalle, Empresa
+
+def iniciarApp():
+    if conection.is_closed():
+        try:
+            conection.connect()
+            print('Conexion Exitosa')
+            
+            conection.create_tables([
+                Empresa,
+                Contenedor,
+                Balance,
+                EntradaDetalle,
+                SalidaDetalle,
+                ContenedorMovimiento,
+                BalanceMovimiento,
+                ContenedorBalance,
+            ])
+        except ex:
+            print(ex)
+
+iniciarApp()
